d9b7885fe5b0928ba78b8d2d00cef2ebdc71dec8
Jhon Vera <rengifovera162@gmail.com>
2021-06-30 18:12:13 -0500

    Modales

--- PORTAFOLIO/PORTAFOLIO_APP/static/js/modal.js
+++ PORTAFOLIO/PORTAFOLIO_APP/static/js/modal.js
@@ -6,6 +6,21 @@ const modalcontainer_electro = document.getElementById('modalcontainer-electro')
 const open_electro = document.getElementById('open-electro');
 const cerrar_electro =  document.getElementById('cerrar-electro');
 
+// Modal-experiencia
+const modalcontainer_experiencia = document.getElementById('modalcontainer-experiencia');
+const open_experiencia = document.getElementById('open-experiencia');
+const cerrar_experiencia =  document.getElementById('cerrar-experiencia');
+
+// Modal-estudios
+const modalcontainer_estudios = document.getElementById('modalcontainer-estudios');
+const open_estudios = document.getElementById('open-estudios');
+const cerrar_estudios =  document.getElementById('cerrar-estudios');
+
+// Modal-resume
+const modalcontainer_resume = document.getElementById('modalcontainer-resume');
+const open_resume = document.getElementById('open-resume');
+const cerrar_resume =  document.getElementById('cerrar-resume');
+
 open.addEventListener('click', () =>{
     modalcontainer.classList.add('show');
     window.addEventListener('scroll', disableScroll);
@@ -17,6 +32,23 @@ open_electro.addEventListener('click', () =>{
 
 }) 
 
+open_experiencia.addEventListener('click', () =>{
+    modalcontainer_experiencia.classList.add('show');
+    window.addEventListener('scroll', disableScroll);
+
+}) 
+
+open_estudios.addEventListener('click', () =>{
+    modalcontainer_estudios.classList.add('show');
+    window.addEventListener('scroll', disableScroll);
+
+}) 
+open_resume.addEventListener('click', () =>{
+    modalcontainer_resume.classList.add('show');
+    window.addEventListener('scroll', disableScroll);
+
+}) 
+
 
 cerrar.addEventListener('click', () =>{
     modalcontainer.classList.remove('show');
@@ -24,6 +56,20 @@ cerrar.addEventListener('click', () =>{
 }) 
 cerrar_electro.addEventListener('click', () =>{
     modalcontainer_electro.classList.remove('show');
+    window.removeEventListener('scroll', disableScroll); 
+}) 
+cerrar_experiencia.addEventListener('click', () =>{
+    modalcontainer_experiencia.classList.remove('show');
+    window.removeEventListener('scroll', disableScroll); 
+}) 
+
+cerrar_estudios.addEventListener('click', () =>{
+    modalcontainer_estudios.classList.remove('show');
+    window.removeEventListener('scroll', disableScroll); 
+}) 
+
+cerrar_resume.addEventListener('click', () =>{
+    modalcontainer_resume.classList.remove('show');
     window.removeEventListener('scroll', disableScroll); 
 }) 
 
